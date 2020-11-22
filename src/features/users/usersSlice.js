b52ce257b0106c21a2472c4a8f8d3d95bb08427d
eviry-ishikawa <ishikawa@eviry.com>
2020-11-22 23:40:47 +0900

    add users slice

---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,17 @@
+const { createSlice } = require("@reduxjs/toolkit")
+
+const initialState = [
+  { id:"0", name: "Tianna Jenkins"},
+  { id:"1", name: "test user 2"},
+  { id:"2", name: "Madison Price"},
+]
+
+const usersSlice = createSlice({
+  name:"users",
+  initialState,
+  reducers:{
+
+  }
+})
+
+export default usersSlice.reducer
