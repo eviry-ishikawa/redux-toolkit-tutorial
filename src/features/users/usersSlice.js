5e565c7a9dc2368a82829e66334f6bc91d291d8a
eviry-ishikawa <ishikawa@eviry.com>
2020-11-29 01:25:31 +0900

    fix usersSlice with EntityAdapter

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,7 +1,9 @@
 import { client } from "../../api/client"
-import  { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import  { createSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit"
 
-const initialState = []
+const usersAdapter = createEntityAdapter()
+
+const initialState = usersAdapter.getInitialState()
 
 export const fetchUsers = createAsyncThunk("users/fetchUsers", async() => {
   const response = await client.get("/fakeApi/users")
@@ -14,14 +16,17 @@ const usersSlice = createSlice({
   reducers:{
   },
   extraReducers:{
-    [fetchUsers.fulfilled]: (state, action) => {
-      return action.payload
-    }
+    [fetchUsers.fulfilled]: usersAdapter.setAll
   }
 })
 
-export const selectAllUsers = state => state.users
+// export const selectAllUsers = state => state.users
 
-export const selectUserById = (state, userId) => state.users.find((user) => user.id === userId)
+// export const selectUserById = (state, userId) => state.users.find((user) => user.id === userId)
+
+export const {
+  selectAll: selectAllUsers,
+  selectById: selectUserById,
+} = usersAdapter.getSelectors(state => state.users)
 
 export default usersSlice.reducer
